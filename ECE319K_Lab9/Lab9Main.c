dcb49841850f546f77d5db343c664bf1fe6b3203
KevinLeeP <kevin.lee.pfeffer@gmail.com>
2026-04-17 11:10:25 -0500

    Raycasting optimizations

--- ECE319K_Lab9/Lab9Main.c
+++ ECE319K_Lab9/Lab9Main.c
@@ -60,6 +60,8 @@ we are honored by your presence
 
 #define joystickDeadBand 50
 
+#define margin F16(0.2)
+
 
 #define F16(x) ((fix16_t)(((x) >= 0) ? ((x) * 65536.0 + 0.5) : ((x) * 65536.0 - 0.5)))
 
@@ -109,21 +111,15 @@ int main(void) { // mainDeadBounds
   ST7735_FillScreen(0);
   ST7735_SetRotation(screenOrientation);
 
-  // fix16_t theta = fix16_div(fix16_pi, fix16_from_int(4));//pi/4
-  // fix16_t sin_theta = fix16_fast_sin(theta);
-
-  // theta = fix16_div(fix16_pi, fix16_from_int(-4));
-  // sin_theta = fix16_fast_sin(theta);
-
-
-
-
   while (1) {
 
-    fix16_t cameraX = 0;
+    fix16_t cameraX = F16(-1);
+    fix16_t cameraXStep = F16(0.0125);
     fix16_t rotSpeed = 0;
     fix16_t walkSpeedX = 0;
     fix16_t walkSpeedY = 0;
+
+
     v2d rayDir = {0, 0};
     
 
@@ -135,7 +131,7 @@ int main(void) { // mainDeadBounds
 
     if(abs(joystickTurnInput) > joystickDeadBand){
       //deltaTheta = joystickTurnInput * pi/ 10000 -> -pi/10 to pi/10
-      rotSpeed = -fix16_div(fix16_mul(fix16_from_int(joystickTurnInput), fix16_pi) ,fix16_from_int(10000));
+      rotSpeed = -fix16_div(fix16_mul(fix16_from_int(joystickTurnInput), fix16_pi) ,F16(10000));
     }
     else{
       rotSpeed = 0;
@@ -169,9 +165,12 @@ int main(void) { // mainDeadBounds
       walkSpeedY = 0;
     }
 
+    // fix16_t moveX = 0;
+    // fix16_t moveY = 0;
+
+    // fix16_t checkX = 0;
+    // fix16_t checkY = 0;
     //check collisions
-
-
     //forward vector
     if(worldMap[ fix16_to_int( pos.x - fix16_mul(dir.x, walkSpeedX))] [ fix16_to_int(pos.y)] == 0){
       pos.x -= fix16_mul(dir.x, walkSpeedX);
@@ -182,9 +181,10 @@ int main(void) { // mainDeadBounds
 
 
     
-    uint32_t testX = fix16_to_int(pos.x);
-    uint32_t testY = fix16_to_int(pos.y - fix16_mul(dir.x , walkSpeedX));
-    uint32_t test = worldMap[ testX ][ testY ];
+    // uint32_t testX = fix16_to_int(pos.x);
+    // uint32_t testY = fix16_to_int(pos.y - fix16_mul(dir.y, walkSpeedX));
+    // uint32_t test = worldMap[ testX ][ testY ];
+
     //perpendicular strafe vector
     //check collisions
     if(worldMap[ fix16_to_int( pos.x + fix16_mul(dir.y, walkSpeedY))] [ fix16_to_int(pos.y)] == 0){
@@ -208,25 +208,29 @@ int main(void) { // mainDeadBounds
 
 
     for (int pixelX = 0; pixelX < screenWidth; pixelX++){
-      cameraX = fix16_div(fix16_mul(fix16_from_int(pixelX), fix16_from_int(2)), fix16_from_int(screenWidth)) - fix16_from_int(1); // (2 * x / screeWidth) - 1
+      //cameraX = fix16_div(fix16_mul(fix16_from_int(pixelX), fix16_from_int(2)), fix16_from_int(screenWidth)) - fix16_from_int(1); // (2 * x / screeWidth) - 1
 
       // rayDir = (plane * cameraX) + dir
       v2d_mul_s(&rayDir, &plane, cameraX);
       v2d_add(&rayDir, &rayDir, &dir);
 
+      cameraX += cameraXStep;
+
       // int32_t mapX = fix16_to_int(pos.x);
       // int32_t mapY = fix16_to_int(pos.y);
 
       //map = [int(pos.x), int(pos.y)]
-      v2d map = {fix16_from_int(fix16_to_int(pos.x)), 
-                 fix16_from_int(fix16_to_int(pos.y))};
+      // v2d map = {fix16_from_int(fix16_to_int(pos.x)), 
+      //            fix16_from_int(fix16_to_int(pos.y))};
+      int32_t mapX  = fix16_to_int(pos.x);
+      int32_t mapY = fix16_to_int(pos.y);
 
 
       fix16_t sideDistX;
       fix16_t sideDistY;
 
-      fix16_t deltaDistX = (rayDir.x == 0) ? fix16_maximum : fix16_abs(fix16_div(fix16_from_int(1), rayDir.x)); //deltaDistX = 1 / |rayDir.x|
-      fix16_t deltaDistY = (rayDir.y == 0) ? fix16_maximum : fix16_abs(fix16_div(fix16_from_int(1), rayDir.y)); //deltaDistY = 1 / |rayDir.y|
+      fix16_t deltaDistX = (rayDir.x == 0) ? fix16_maximum : fix16_abs(fix16_div(F16(1), rayDir.x)); //deltaDistX = 1 / |rayDir.x|
+      fix16_t deltaDistY = (rayDir.y == 0) ? fix16_maximum : fix16_abs(fix16_div(F16(1), rayDir.y)); //deltaDistY = 1 / |rayDir.y|
       fix16_t perpWallDist;
 
       int8_t stepX;
@@ -239,20 +243,20 @@ int main(void) { // mainDeadBounds
 
       if(rayDir.x < 0){
         stepX = -1;
-        sideDistX = fix16_mul((pos.x - map.x), deltaDistX);
+        sideDistX = fix16_mul((pos.x - (mapX << 16 )), deltaDistX);
       }
       else{
         stepX = 1;
-        sideDistX = fix16_mul((map.x + fix16_from_int(1) - pos.x), deltaDistX);
+        sideDistX = fix16_mul(((mapX << 16) + F16(1) - pos.x), deltaDistX);
       }
 
       if(rayDir.y < 0){
         stepY = -1;
-        sideDistY = fix16_mul((pos.y - map.y), deltaDistY);
+        sideDistY = fix16_mul((pos.y - (mapY << 16)), deltaDistY);
       }
       else{
         stepY = 1;
-        sideDistY = fix16_mul((map.y + fix16_from_int(1) - pos.y), deltaDistY);
+        sideDistY = fix16_mul(( (mapY << 16) + F16(1) - pos.y), deltaDistY);
       }
 
 
@@ -260,16 +264,16 @@ int main(void) { // mainDeadBounds
           
           if(sideDistX < sideDistY){
             sideDistX += deltaDistX;
-            map.x += fix16_from_int(stepX);
+            mapX += stepX;
             sideHit = 0;
           }
           else{
             sideDistY += deltaDistY;
-            map.y += fix16_from_int(stepY);
+            mapY += stepY;
             sideHit = 1;
           }
 
-          if(worldMap[fix16_to_int(map.x)][fix16_to_int(map.y)] != 0){
+          if(worldMap[mapX][mapY] != 0){
             wallHit = 1;
           }
 
@@ -283,15 +287,15 @@ int main(void) { // mainDeadBounds
       }
 
       
-      int32_t lineHeight = fix16_to_int(fix16_mul(fix16_div(fix16_from_int(1), perpWallDist), fix16_from_int(wallHeight))); //CHECK THIS
-      int32_t drawWallStart = fix16_to_int(fix16_div(fix16_from_int(screenHeight),fix16_from_int(2)) 
-                                      - fix16_div(fix16_from_int(lineHeight), fix16_from_int(2))); // drawStart = (screenHeight/2) - (lineHeight/2)
+      int32_t lineHeight = (wallHeight << 16) / perpWallDist;//fix16_to_int(fix16_mul(fix16_div(fix16_from_int(1), perpWallDist), fix16_from_int(wallHeight))); //CHECK THIS
+      int32_t drawWallStart = fix16_to_int(fix16_div(F16(screenHeight),F16(2)) 
+                                      - fix16_div(fix16_from_int(lineHeight), F16(2))); // drawStart = (screenHeight/2) - (lineHeight/2)
 
-      int32_t drawSkyEnd = fix16_to_int(fix16_div(fix16_from_int(screenHeight), fix16_from_int(2)) 
-                                      - fix16_div(fix16_from_int(lineHeight), fix16_from_int(2))
+      int32_t drawSkyEnd = fix16_to_int(fix16_div(F16(screenHeight), F16(2)) 
+                                      - fix16_div(fix16_from_int(lineHeight), F16(2))
                                       ); //(screenHeight/2) - (lineHeight/2)
-      int32_t drawFloorStart = fix16_to_int(fix16_div(fix16_from_int(screenHeight), fix16_from_int(2)) 
-                                      + fix16_div(fix16_from_int(lineHeight), fix16_from_int(2))
+      int32_t drawFloorStart = fix16_to_int(fix16_div(F16(screenHeight), F16(2)) 
+                                      + fix16_div(fix16_from_int(lineHeight), F16(2))
                                       );    //(screenHeight/2) + (lineHeight/2)
 
       if(drawWallStart < 0){
@@ -315,7 +319,7 @@ int main(void) { // mainDeadBounds
       }
 
       uint16_t color;
-      switch(worldMap[fix16_to_int(map.x)][fix16_to_int(map.y)]){
+      switch(worldMap[mapX][mapY]){
         case 1: 
         color = ST7735_RED; break;
         case 2:
@@ -338,6 +342,8 @@ int main(void) { // mainDeadBounds
 
       // ST7735_SetCursor(0,0);
       // ST7735_OutString("X=");
+      //ST7735_DrawCharS(0,0, (fix16_to_int(pos.x) / 10) + 0x30, ST7735_CYAN, ST7735_BLACK, 1);
+      // ST7735_OutChar((fix16_to_int(pos.x) % 10) + 0x30);
       // ST7735_SetCursor(2, 0);
       // ST7735_OutUDec4(fix16_to_int(pos.x));
     }
