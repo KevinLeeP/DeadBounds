6f4c16c3de6f095600a8408201aed38b49e94e0d
charlie <charlien439@gmail.com>
2026-04-17 19:17:53 -0500

    Added ST7735_DrawBitmapTransparent() function and added some sprites

--- inc/ST7735.h
+++ inc/ST7735.h
@@ -912,6 +912,7 @@ void ST7735_Line(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2,
 */
 void ST7735_SetX(int32_t newX);
 
+void ST7735_DrawBitmapTransparent(int16_t x, int16_t y, const uint16_t *image, int16_t w, int16_t h);
 
 #endif
 /** @}*/
